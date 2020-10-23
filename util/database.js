902c54c3bf347227ae81d4bd13edf43d45f46a8a
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-10-23 22:19:41 +0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   dialect: config.dialect,
   host: config.host,
   port: config.port,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 
